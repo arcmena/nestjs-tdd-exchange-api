4a279019ab0d30f1c6b06792511cfbeac0ca36c6
Marcelo Mena <marcelomena010@gmail.com>
2020-10-24 16:03:52 -0300

    Ensure that getCurrency is called twice, one for each param

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -1,33 +1,50 @@
 import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { ExchangeService } from './exchange.service';
+import { CurrenciesService, ExchangeService } from './exchange.service';
 
 describe('ExchangeService', () => {
-  let service: ExchangeService;
+    let service: ExchangeService;
+    let currenciesService: CurrenciesService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ExchangeService],
-    }).compile();
+    beforeEach(async () => {
+        const currenciesServiceMock = {
+            getCurrency: jest.fn(),
+        };
 
-    service = module.get<ExchangeService>(ExchangeService);
-  });
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                ExchangeService,
+                {
+                    provide: CurrenciesService,
+                    useFactory: () => currenciesServiceMock,
+                },
+            ],
+        }).compile();
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-
-  describe('convertAmount()', () => {
-    it('should be thrown if called with invalid params', async () => {
-      await expect(
-        service.convertAmount({ from: '', to: '', amount: 0 }),
-      ).rejects.toThrow(new BadRequestException());
+        service = module.get<ExchangeService>(ExchangeService);
+        currenciesService = module.get<CurrenciesService>(CurrenciesService);
     });
 
-    it('should not be thrown if called with valid params', async () => {
-      await expect(
-        service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 }),
-      ).resolves.not.toThrow();
+    it('should be defined', () => {
+        expect(service).toBeDefined();
     });
-  });
+
+    describe('convertAmount()', () => {
+        it('should be thrown if called with invalid params', async () => {
+            await expect(
+                service.convertAmount({ from: '', to: '', amount: 0 }),
+            ).rejects.toThrow(new BadRequestException());
+        });
+
+        it('should not be thrown if called with valid params', async () => {
+            await expect(
+                service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 }),
+            ).resolves.not.toThrow();
+        });
+
+        it('should be called getCurrency twice', async () => {
+            await service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 }),
+                expect(currenciesService.getCurrency).toBeCalledTimes(2);
+        });
+    });
 });
--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -1,8 +1,21 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 
+export class CurrenciesService {
+    async getCurrency(currency: string): Promise<any> {
+        //
+    }
+}
+
 @Injectable()
 export class ExchangeService {
-  async convertAmount({ from, to, amount }): Promise<any> {
-    if (!from || !to || !amount) throw new BadRequestException();
-  }
+    constructor(private currenciesService: CurrenciesService) {
+        //
+    }
+
+    async convertAmount({ from, to, amount }): Promise<any> {
+        if (!from || !to || !amount) throw new BadRequestException();
+
+        const currencyFrom = this.currenciesService.getCurrency(from);
+        const currencyTo = this.currenciesService.getCurrency(to);
+    }
 }
