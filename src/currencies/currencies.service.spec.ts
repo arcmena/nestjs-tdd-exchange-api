0902c1d94433b6c619c98627997d918effafcf44
Marcelo Mena <marcelomena010@gmail.com>
2020-10-24 18:04:52 -0300

    Ensure getcurrency throws if repository throws

--- src/currencies/currencies.service.spec.ts
+++ src/currencies/currencies.service.spec.ts
@@ -1,18 +1,45 @@
+import { InternalServerErrorException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { CurrenciesService } from './currencies.service';
+import { CurrenciesRepository, CurrenciesService } from './currencies.service';
 
 describe('CurrenciesService', () => {
-  let service: CurrenciesService;
+    let service: CurrenciesService;
+    let repository: CurrenciesRepository;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CurrenciesService],
-    }).compile();
+    beforeEach(async () => {
+        const currenciesRepositoryMock = {
+            getCurrency: jest.fn(),
+        };
 
-    service = module.get<CurrenciesService>(CurrenciesService);
-  });
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                CurrenciesService,
+                {
+                    provide: CurrenciesRepository,
+                    useFactory: () => currenciesRepositoryMock,
+                },
+            ],
+        }).compile();
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+        service = module.get<CurrenciesService>(CurrenciesService);
+        repository = module.get<CurrenciesRepository>(CurrenciesRepository);
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    describe('getCurrency()', () => {
+        it('should throw if repository throw', async () => {
+            (repository.getCurrency as jest.Mock).mockRejectedValue(
+                new InternalServerErrorException(),
+            );
+            await expect(service.getCurrency('INVALID')).rejects.toThrow(
+                new InternalServerErrorException(),
+            );
+        });
+        it('should not throw if repository returns', async () => {
+            await expect(service.getCurrency('USD')).resolves.not.toThrow();
+        });
+    });
 });
