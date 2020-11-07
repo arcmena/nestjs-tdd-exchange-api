439b8f7dd8181f1130246e72f535c04e5a429b61
Marcelo Mena <marcelomena010@gmail.com>
2020-11-07 14:44:14 -0300

    Organize database config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// Modules
 import { ExchangeModule } from './exchange/exchange.module';
 import { CurrenciesModule } from './currencies/currencies.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { configService } from './config/config.service';
+
+// DB Configuration
+import { configService } from './database/database.service';
 
 @Module({
     imports: [
